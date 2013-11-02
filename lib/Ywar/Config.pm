de35e8d77cfb183d434aa0a02ce2124420978ec4
Ricardo Signes <rjbs@cpan.org>
2013-11-02 11:48:36 -0400

    drop the file extension on config file

--- lib/Ywar/Config.pm
+++ lib/Ywar/Config.pm
@@ -3,14 +3,14 @@ use warnings;
 package Ywar::Config;
 
 use Carp qw(confess);
-use YAML::XS 'LoadFile';
+use YAML::XS ();
 
 sub config {
   my ($self) = @_;
 
   state $config = do {
-    my $fn = $ENV{YWAR_CONFIG_FILE} || "$ENV{HOME}/.ywar.yml";
-    LoadFile($fn);
+    my $fn = $ENV{YWAR_CONFIG_FILE} || "$ENV{HOME}/.ywar";
+    YAML::XS::LoadFile($fn);
   };
 
   return $config;
