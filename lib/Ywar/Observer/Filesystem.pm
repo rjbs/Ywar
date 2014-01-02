00a803e8b2c13712cacf2743c117bd144a05a89f
Ricardo Signes <rjbs@cpan.org>
2014-01-01 21:49:43 -0500

    update Filesystem observer in a few ways:
    1. recurse through subdirectories
    2. fix warning text, which was too specific
    3. add a check for multiple directories

--- lib/Ywar/Observer/Filesystem.pm
+++ lib/Ywar/Observer/Filesystem.pm
@@ -2,14 +2,34 @@ use 5.14.0;
 package Ywar::Observer::Filesystem;
 use Moose;
 
+use Path::Iterator::Rule;
+
 sub more_files_in_dir {
   my ($self, $prev, $arg) = @_;
   my $dir = $arg->{dir};
 
-  my $count = grep {; -f $_ } <$dir/*>;
+  my $count = Path::Iterator::Rule->file->all($dir);
 
   my $last = $prev->{measured_value};
-  warn "fewer openers today ($count) than last time ($last)\n"
+  warn "fewer files today ($count) than last time ($last)\n"
+    if $count < $last;
+
+  return {
+    value    => $count,
+    met_goal => $count > $last ? 1 : 0,
+    note     => "files added: " . ($count - $last),
+  };
+}
+
+# not sure I'm happy with this being a distinct method
+sub more_files_across_dirs {
+  my ($self, $prev, $arg) = @_;
+  my $dirs = $arg->{dirs};
+
+  my $count = Path::Iterator::Rule->file->all(@$dirs);
+
+  my $last = $prev->{measured_value};
+  warn "fewer files today ($count) than last time ($last)\n"
     if $count < $last;
 
   return {
