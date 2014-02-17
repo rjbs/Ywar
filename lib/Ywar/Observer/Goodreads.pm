e1da2c5e00900006ef8128aa1e347737cb28f036
Ricardo Signes <rjbs@cpan.org>
2014-02-16 22:43:13 -0500

    total rewrite of Goodreads observer

--- lib/Ywar/Observer/Goodreads.pm
+++ lib/Ywar/Observer/Goodreads.pm
@@ -1,98 +1,125 @@
-use 5.14.0;
+use 5.16.0;
 package Ywar::Observer::Goodreads;
 use Moose;
 
+use JSON 2 ();
 use LWP::UserAgent;
-use XML::Tiny;
-use JSON 'decode_json', 'encode_json';
-use DateTime::Format::ISO8601;
+use List::AllUtils 'uniq';
+use XML::LibXML;
 
 has api_key => (
-   is => 'ro',
-   required => 1,
+  is => 'ro',
+  required => 1,
 );
 
 has user_id => (
-   is => 'ro',
-   required => 1,
+  is => 'ro',
+  required => 1,
 );
 
-has _statuses => (
-   is => 'ro',
-   lazy => 1,
-   builder => '_get_statuses',
+has lwp => (
+  is => 'ro',
+  default => sub { LWP::UserAgent->new(keep_alive => 2) },
 );
 
-sub _get_statuses {
-  my $self = shift;
+my $BASE = 'https://www.goodreads.com';
 
-  my $ua = LWP::UserAgent->new(keep_alive => 1);
+my $JSON = JSON->new;
 
-  my $response = $ua->get(
-    sprintf 'https://www.goodreads.com/user/show/%d.xml?key=%s',
-      $self->user_id, $self->api_key,
+sub read_pages_on_shelf {
+  my ($self, $laststate, $arg) = @_;
+
+  unless ($arg->{goal_pages}) { warn "no goal pages set!"; return; }
+  unless ($arg->{shelf})      { warn "no shelf selected!"; return; }
+
+  my $old  = $JSON->decode( $laststate->{measured_value} );
+
+  my $res = LWP::UserAgent->new->get(
+    sprintf 'https://www.goodreads.com/review/list?format=json&v=2&id=%s&key=%s&shelf=currently-reading',
+      $self->user_id,
+      $self->api_key,
   );
 
-  my $data = XMLin($response->decoded_content);
+  my @review_ids = uniq(
+    (map {; $_->{id} } @{ $JSON->decode($res->decoded_content) }),
+    keys %$old,
+  );
 
-  $data->{user}{updates}{update};
-}
-
-sub did_read {
-  my ($self, $prev, $args) = @_;
-
-  warn "title match: $args->{match_title}";
-
-  use Devel::Dwarn;
-  my $now = DateTime->now->epoch;
-  my @important_data =
-  grep { $now - $_->{created_at}->epoch < 24 * 60 * 60 }
-  map {
-    my $us = $_->{object}{user_status};
-    my $b = $us->{book};
-    +{
-      title => $b->{title},
-      book_id => $us->{book_id}{content},
-      created_at => DateTime::Format::ISO8601->parse_datetime($us->{created_at}{content}),
-      current_page => $us->{page}{content},
-      total_pages => $b->{num_pages}{content},
-    }
-  }
-  grep $_->{object}{user_status}{book}->{title}->$Dwarn =~ m/$args->{match_title}/,
-  grep $_->{action_text} =~ m/^is on page \d+ of \d+/,
-  @{$self->_statuses};
-
-  my %progress      = map { $_->{book_id} => $_ } @important_data;
-  my %prev_progress = map { $_->{book_id} => $_ } @{decode_json($prev->{measured_value})};
-  my %new_progress;
-
-  for (keys %progress) {
-      if (!exists $prev_progress{$_}) {
-         $new_progress{$_} = $progress{$_};
-      } elsif ($prev_progress{$_} < $progress{$_}) {
-         $new_progress{$_} = [$progress{$_}, $prev_progress{$_}];
-      } elsif ($progress{$_} < $prev_progress{$_}) {
-         die "why are you reading backwards you dummy?\n"
-      }
+  my %current;
+  REVIEW: for my $review_id (@review_ids) {
+    my $status = $self->_get_review_status($review_id);
+    next REVIEW unless grep {; fc $_ eq fc $arg->{shelf} } @{$status->{shelves}};
+    $current{ $review_id } = $status;
   }
 
-  for (keys %prev_progress) {
-      if (!exists $progress{$_}) {
-         $new_progress{$_} = 'complete!'
-      }
+  my %to_save;
+  my $total_diff;
+  my @notes;
+  for my $id (keys %current) {
+    my $status = $current{$id};
+    my $diff   = $status->{current_page} - ($old->{$id} // 0);
+    $diff = 0 if $diff < 0;
+    $total_diff += $diff;
+    push @notes, "read $diff pages in $status->{title}" if $diff;
+    $to_save{$id} = $status->{current_page}
+      if grep { fc $_ eq 'currently-reading' } @{ $status->{shelves} };
   }
 
-  $progress{$_}{created_at} = "$progress{$_}{created_at}"
-    for keys %progress;
-  Ddie \%progress;
   return {
-    note => ,
-    value => encode_json(\%progress),
-    met_goal => scalar %new_progress ? 1 : 0,
+    note  => join(q{; }, @notes),
+    value => $JSON->encode(\%to_save),
+    met_goal => $total_diff >= $arg->{goal_pages},
   };
 }
 
-1;
+sub _get_review_status {
+  my ($self, $review_id) = @_;
+
+  my %status;
+
+  my $res = LWP::UserAgent->new->get(
+    sprintf 'https://www.goodreads.com/review/show.xml?key=%s&id=%s',
+      $self->api_key,
+      $review_id,
+  );
+
+  open my $fh, '<', \$res->decoded_content(charset => 'none')
+    or die "error making handle to XML results: $!";
+
+  my $doc = XML::LibXML->load_xml(IO => $fh);
+
+  $status{shelves} = [
+    map {; $_->getAttribute('name') } $doc->getElementsByTagName('shelf')
+  ];
+
+  my $title = ($doc->getElementsByTagName('title'))[0]->textContent;
+
+  my ($total_page_node) = $doc->getElementsByTagName('num_pages');
+  my $page_count;
+  unless ($total_page_node and ($page_count = $total_page_node->textContent)) {
+    warn "couldn't figure out page count for book on review $review_id";
+    next REVIEW;
+  }
+
+  $status{page_count} = $page_count;
+
+  my ($latest) = $doc->getElementsByTagName('user_status');
+
+  my $page = 0;
+  if ($latest) {
+    my ($page_node) = $latest->getElementsByTagName('page');
+    my ($pct_node)  = $latest->getElementsByTagName('percent');
+
+    $page = int(($page_node->getAttribute('nil') // 'false') eq 'false'
+              ? $page_node->textContent
+              : ($page_count * ($pct_node->textContent / 100)));
+  }
+
+  $status{current_page} = $page;
+  $status{title} = $title;
+
+  return \%status;
+}
 
 __END__
 
@@ -101,14 +128,16 @@ __END__
         api_key: foo
         user_id: baz
      checks:
-         read.acm:
-             method: did_read
-             tdp-id: 123
-             args:
-                match_title: Communcations of the ACM
-         read.imbibe:
-             method: did_read
-             tdp-id: 124
-             args:
-                match_title: Imbibe
+       read.tech:
+           method: read_pages_on_shelf
+           tdp-id: 123
+           args:
+              shelf: tech
+              goal_pages: 25
+       read.literature:
+           method: read_pages_on_shelf
+           tdp-id: 124
+           args:
+              shelf: lit
+              goal_pages: 50
 
