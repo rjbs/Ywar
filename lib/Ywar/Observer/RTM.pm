e6a2e65cb2c38c632de19630e2fa1420b1b1addc
Ricardo Signes <rjbs@cpan.org>
2015-03-01 13:32:56 -0500

    fix totally busted closed-something-from-yesterday counting

--- lib/Ywar/Observer/RTM.pm
+++ lib/Ywar/Observer/RTM.pm
@@ -2,8 +2,10 @@ use 5.14.0;
 package Ywar::Observer::RTM;
 use Moose;
 
+use DateTime::Format::ISO8601;
 use List::Util qw(sum0);
 use WebService::RTMAgent;
+use Ywar::Logger '$Logger';
 use Ywar::Util qw(not_today);
 
 has [ qw(api_key api_secret) ] => (is => 'ro', required => 1);
@@ -46,41 +48,46 @@ sub nothing_overdue {
 sub closed_old_tasks {
   my ($self, $laststate) = @_;
 
-  my %count;
+  my $yesterday_14 = $laststate->yesterday_value->{measured_value};
 
-  for my $age (
-    [ last  => $laststate->completion->{measured_at} ],
-    [ today => $^T ],
-  ) {
-    my $date = DateTime->from_epoch(epoch => $age->[1])
-                       ->subtract(days => 14)
-                       ->format_cldr("yyyy-MM-dd");
+  my $old_date = DateTime->today(time_zone => Ywar::Config->time_zone)
+                         ->subtract(days => 14);
 
-    my $filter = "status:incomplete AND addedBefore:$date"
-               . " AND due:never AND NOT tag:nag";
+  my $filter = "status:incomplete AND addedBefore:"
+             . $old_date->format_cldr('yyyy-MM-dd')
+             . " AND due:never AND NOT tag:nag";
 
-    my $res = $self->rtm_ua->tasks_getList("filter=$filter");
+  my $res = $self->rtm_ua->tasks_getList("filter=$filter");
 
-    unless ($res) {
-      warn "RTM API error: " . $self->rtm_ua->error;
-      return;
-    }
-
-    my @series = @{ $res->{tasks}[0]{list} || [] };
-    $count{ $age->[0] } = sum0 map {; scalar @{ $_->{taskseries} } } @series;
+  unless ($res) {
+    warn "RTM API error: " . $self->rtm_ua->error;
+    return;
   }
 
-  my $last = $laststate->completion->{measured_value};
+  my @tasks = map {; @{ $_->{taskseries} } }
+              @{ $res->{tasks}[0]{list} || [] };
 
-  my %result = (value => $count{today});
+  my $today_14 = 0;
+  my $today_15 = 0;
 
-  # warn "LAST: $last / C-LAST: $count{last} /  NOW: $count{today}\n";
+  for my $task (@tasks) {
+    my $created = DateTime::Format::ISO8601
+                    ->parse_datetime($task->{created});
 
-  if ($count{last} == 0 || $count{last} < $last) {
-    my $closed = $last - $count{last};
+    $today_14++;
+    $today_15++ if $created < $old_date;
+  }
+
+  my %result = (value => $today_14);
+
+  my $closed = 0;
+  if ($today_15 < $yesterday_14) {
+    $closed = $yesterday_14 - $today_15;
     $result{note} = "items closed: $closed";
     $result{met_goal} = not_today($laststate->completion);
   }
+
+  $Logger->log("RTM tasks: yesterday's count: $yesterday_14; closed: $closed; today: $today_14");
 
   return \%result;
 }
